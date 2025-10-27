1ba4eb31684ee443c7f8df733f0e05ee78f4dafe
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-10-27 10:50:43 +0800

    Memperbaiki drag mouse, dan lainnya

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -83,104 +83,77 @@
             <img class="absolute top-40 right-0 rounded-xl outline-4 outline-lime-600/15" width="800"
                 src="img/placeholder.jpg" alt="img-peninsula-island">
             <!-- /Landing Page -->
+            
+                {{-- Maps --}}
+        <div class="mt-32 mb-32">
+            <p class="text-6xl font-semibold text-center text-lime-600 mb-10">
+                Explore <span class="text-light-primary">What'Inside</span>
+            </p>
+
+            <div class="relative max-w-5xl h-[30rem] mx-auto mb-32 overflow-hidden rounded-2xl shadow-lg bg-gray-200">
+                <div id="mapContainer" class="w-full h-full cursor-grab active:cursor-grabbing" onmousedown="return false">
+                    <img src="./img/map.jpeg" alt="Map" class="w-full h-full object-cover">
+                </div>
+
+                <div class="absolute bottom-4 right-4 flex flex-col space-y-2">
+                    <button id="zoomIn" class="bg-white border rounded-full p-2 shadow hover:bg-gray-100">+</button>
+                    <button id="zoomOut" class="bg-white border rounded-full p-2 shadow hover:bg-gray-100">−</button>
+                </div>
+            </div>
         </div>
-    </div>
 
+        {{-- Where Location --}}
+        <div class="max-w-6xl">
+            <div class="flex items-center justify-between">
+                <p class="text-6xl font-semibold">
+                    Where's
+                    <br><span class="text-lime-600">The Location</span>
+                </p>
 
-    {{-- Maps --}}
-    <div class="mt-32 mb-32">
-        <p class="text-6xl font-semibold text-center text-lime-600 mb-10">
-            Explore <span class="text-light-primary">What'Inside</span>
-        </p>
-
-        <div class="relative max-w-5xl h-[30rem] mx-auto mb-32 overflow-hidden rounded-2xl shadow-lg bg-gray-200">
-            <div id="mapContainer" class="w-full h-full cursor-grab active:cursor-grabbing">
-                <img src="./img/map.jpeg" alt="Map" class="w-full h-full object-cover">
+                <button class="bg-gray-400 px-8 py-4 rounded-xl hover:bg-gray-500 transition flex items-center gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
+                        <path fill="#48b564"
+                            d="M35.76,26.36h0.01c0,0-3.77,5.53-6.94,9.64c-2.74,3.55-3.54,6.59-3.77,8.06	C24.97,44.6,24.53,45,24,45s-0.97-0.4-1.06-0.94c-0.23-1.47-1.03-4.51-3.77-8.06c-0.42-0.55-0.85-1.12-1.28-1.7L28.24,22l8.33-9.88	C37.49,14.05,38,16.21,38,18.5C38,21.4,37.17,24.09,35.76,26.36z">
+                        </path>
+                        <path fill="#fcc60e"
+                            d="M28.24,22L17.89,34.3c-2.82-3.78-5.66-7.94-5.66-7.94h0.01c-0.3-0.48-0.57-0.97-0.8-1.48L19.76,15	c-0.79,0.95-1.26,2.17-1.26,3.5c0,3.04,2.46,5.5,5.5,5.5C25.71,24,27.24,23.22,28.24,22z">
+                        </path>
+                        <path fill="#2c85eb"
+                            d="M28.4,4.74l-8.57,10.18L13.27,9.2C15.83,6.02,19.69,4,24,4C25.54,4,27.02,4.26,28.4,4.74z">
+                        </path>
+                        <path fill="#ed5748"
+                            d="M19.83,14.92L19.76,15l-8.32,9.88C10.52,22.95,10,20.79,10,18.5c0-3.54,1.23-6.79,3.27-9.3	L19.83,14.92z">
+                        </path>
+                        <path fill="#5695f6"
+                            d="M28.24,22c0.79-0.95,1.26-2.17,1.26-3.5c0-3.04-2.46-5.5-5.5-5.5c-1.71,0-3.24,0.78-4.24,2L28.4,4.74	c3.59,1.22,6.53,3.91,8.17,7.38L28.24,22z">
+                        </path>
+                    </svg>
+                    <span class="text-sm">Take me to Google Maps</span>
+                    <img width="20" height="20"
+                        src="https://img.icons8.com/external-anggara-basic-outline-anggara-putra/24/external-share-ui-anggara-basic-outline-anggara-putra.png"
+                        alt="external-share-ui-anggara-basic-outline-anggara-putra" />
+                </button>
             </div>
 
+            <p class="mt-6 max-w-3xl">
+                Peninsula Island is located in Nusa Dua, on the southeastern coast of Bali, Indonesia.
+                It sits within the ITDC (Indonesia Tourism Development Corporation) complex,
+                a gated resort area known for its luxury hotels and pristine beaches.
+            </p>
+        </div>
+
+        <div class="relative max-w-6xl h-[30rem] mt-10 mx-auto mb-32 overflow-hidden rounded-2xl shadow-lg bg-gray-200">
+            <div id="mapContainer" class="w-full h-full cursor-grab active:cursor-grabbing" onmousedown="return false">
+                <img src="./img/maps.png" alt="Map" class="w-full h-full object-cover">
+            </div>
+
+            {{-- Tombol --}}
             <div class="absolute bottom-4 right-4 flex flex-col space-y-2">
                 <button id="zoomIn" class="bg-white border rounded-full p-2 shadow hover:bg-gray-100">+</button>
                 <button id="zoomOut" class="bg-white border rounded-full p-2 shadow hover:bg-gray-100">−</button>
             </div>
         </div>
     </div>
-
-    {{-- Where Location --}}
-    <div class="max-w-6xl mx-20">
-        <div class="flex items-center justify-between">
-            <p class="text-6xl font-semibold">
-                Where's
-                <br><span class="text-lime-600">The Location</span>
-            </p>
-
-            <button class="bg-gray-400 px-8 py-4 rounded-xl hover:bg-gray-500 transition flex items-center gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
-                    <path fill="#48b564"
-                        d="M35.76,26.36h0.01c0,0-3.77,5.53-6.94,9.64c-2.74,3.55-3.54,6.59-3.77,8.06	C24.97,44.6,24.53,45,24,45s-0.97-0.4-1.06-0.94c-0.23-1.47-1.03-4.51-3.77-8.06c-0.42-0.55-0.85-1.12-1.28-1.7L28.24,22l8.33-9.88	C37.49,14.05,38,16.21,38,18.5C38,21.4,37.17,24.09,35.76,26.36z">
-                    </path>
-                    <path fill="#fcc60e"
-                        d="M28.24,22L17.89,34.3c-2.82-3.78-5.66-7.94-5.66-7.94h0.01c-0.3-0.48-0.57-0.97-0.8-1.48L19.76,15	c-0.79,0.95-1.26,2.17-1.26,3.5c0,3.04,2.46,5.5,5.5,5.5C25.71,24,27.24,23.22,28.24,22z">
-                    </path>
-                    <path fill="#2c85eb"
-                        d="M28.4,4.74l-8.57,10.18L13.27,9.2C15.83,6.02,19.69,4,24,4C25.54,4,27.02,4.26,28.4,4.74z">
-                    </path>
-                    <path fill="#ed5748"
-                        d="M19.83,14.92L19.76,15l-8.32,9.88C10.52,22.95,10,20.79,10,18.5c0-3.54,1.23-6.79,3.27-9.3	L19.83,14.92z">
-                    </path>
-                    <path fill="#5695f6"
-                        d="M28.24,22c0.79-0.95,1.26-2.17,1.26-3.5c0-3.04-2.46-5.5-5.5-5.5c-1.71,0-3.24,0.78-4.24,2L28.4,4.74	c3.59,1.22,6.53,3.91,8.17,7.38L28.24,22z">
-                    </path>
-                </svg>
-                <span class="text-sm">Take me to Google Maps</span>
-                <img width="20" height="20"
-                    src="https://img.icons8.com/external-anggara-basic-outline-anggara-putra/24/external-share-ui-anggara-basic-outline-anggara-putra.png"
-                    alt="external-share-ui-anggara-basic-outline-anggara-putra" />
-            </button>
-        </div>
-
-        <p class="mt-6 max-w-3xl">
-            Peninsula Island is located in Nusa Dua, on the southeastern coast of Bali, Indonesia.
-            It sits within the ITDC (Indonesia Tourism Development Corporation) complex,
-            a gated resort area known for its luxury hotels and pristine beaches.
-        </p>
-    </div>
-
-    <div class="relative max-w-6xl h-[30rem] mt-10 mx-auto mb-32 overflow-hidden rounded-2xl shadow-lg bg-gray-200">
-        <div id="mapContainer" class="w-full h-full cursor-grab active:cursor-grabbing">
-            <img src="./img/maps.png" alt="Map" class="w-full h-full object-cover">
-        </div>
-
-        {{-- Tombol --}}
-        <div class="absolute bottom-4 right-4 flex flex-col space-y-2">
-            <button id="zoomIn" class="bg-white border rounded-full p-2 shadow hover:bg-gray-100">+</button>
-            <button id="zoomOut" class="bg-white border rounded-full p-2 shadow hover:bg-gray-100">−</button>
-        </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     {{-- gatau dah yang ini --}}
     <script>
