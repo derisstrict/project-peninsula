ed76145cec93e7507cb3469bea7d079a4b48e8cf
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-10-29 18:29:41 +0800

    Minor fix to icon's color

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -32,8 +32,8 @@
             </div>
             <div class="flex bg-light-primary/5 w-fit items-center pl-3 rounded-xl p-2 gap-2 dark:bg-dark-primary/5">
                 <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path d="M4 7.00005L10.2 11.65C11.2667 12.45 12.7333 12.45 13.8 11.65L20 7" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                <rect x="3" y="5" width="18" height="14" rx="2" stroke="#000000" stroke-width="2" stroke-linecap="round"/>
+                <path d="M4 7.00005L10.2 11.65C11.2667 12.45 12.7333 12.45 13.8 11.65L20 7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <rect x="3" y="5" width="18" height="14" rx="2" stroke-width="2" stroke-linecap="round"/>
                 </svg>
                 <input type="text" id="email" class="w-80 outline-none" placeholder="Email">
             </div>
