5c36a451a237e933972a839baa0edf5e0bc0db50
radityakusumaa <kusumaraditya074@gmail.com>
2025-10-29 21:44:58 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -240,4 +240,17 @@ window.addEventListener('mouseup', () => {
     </div>
 </div> --}}
 
+
+<!--Things You Can Do-->
+<section class="container mx-auto mt-32 flex flex-col items-center text-center">
+    <div>
+        <p class="text-6xl font-semibold text-lime-600 mb-10">
+            Things <span class="text-light-primary">You Can Do</span>
+        </p>
+        <p class="mt-6 max-w-4xl text-xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur recusandae dolor et, explicabo ipsum deleniti? 
+            Temporibus fuga corrupti, sit porro voluptatibus suscipit? Corrupti alias quasi nesciunt ratione harum praesentium eius?</p>
+    </div>
+</section>
+
+
 @endsection
