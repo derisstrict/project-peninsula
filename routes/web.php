0cad2055eb0d519769b959a0d6ab5f3a21a6be96
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-10-29 14:30:10 +0800

    Updated the routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/blogs', function () {
+    return view('blogs');
+});
+
+Route::get('/events', function () {
+    return view('events');
+});
+
+Route::get('/events/events-detail', function () {
+    return view('events-detail');
+});
+
+Route::get('/blogs/blogs-detail', function () {
+    return view('blogs-detail');
+});
