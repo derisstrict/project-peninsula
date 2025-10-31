60b52cd1f812928b1881fba005d920de91284cb2
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-10-31 11:17:17 +0800

    Changed the blog's card appearance

--- resources/views/components/blogs-card.blade.php
+++ resources/views/components/blogs-card.blade.php
@@ -1,11 +1,15 @@
-<a {{ $attributes }} class="flex flex-col gap-2 p-5 rounded-xl hover:bg-light-primary/5 dark:hover:bg-dark-primary/5 transition-colors">
-    <img class="rounded-lg" src="{{ $image }}" alt="image">
-    <p class="text-lg font-semibold">{{ $slot }}</p>
-    <div class="bg-lime-500/25 outline-2 outline-lime-500/50 w-fit rounded-lg px-2 text-sm">Other</div>
-    <div class="flex items-center">
-        <p class="text-sm mr-auto">{{ $date }}</p>
-        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path d="M7 17L17 7M17 7H8M17 7V16" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
+<a {{ $attributes }} class="flex gap-5 p-5 rounded-xl hover:bg-light-primary/5 dark:hover:bg-dark-primary/5 transition-colors">
+    <img class="rounded-lg w-80 h-60 content-cover" src="{{ $image }}" alt="image">
+    <div class="flex flex-col gap-2 w-full">
+        <p class="text-2xl font-semibold">{{ $title }}</p>
+        <div class="bg-lime-500/25 outline-2 outline-lime-500/50 w-fit rounded-lg px-2 text-sm">Other</div>
+        <p class="text-sm mr-auto">{{ $date }}</p>        
+        <p>{{ Str::limit($slot, 300) }}</p>
+        <div class="flex items-center mt-auto gap-3">
+            <p class="text-sm ml-auto">Read more</p>
+            <svg width="24px" height="24px" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7 17L17 7M17 7H8M17 7V16" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </div>
     </div>
 </a>
