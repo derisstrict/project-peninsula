39589a5131f61cb16f281d43561c966e2c1de4c8
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-11-04 01:05:16 +0800

    Initial content to overview page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,4 +6,5 @@
 <div class="relative">
     <div class="background-radial-blur-dashboard top-0 inset-0 -translate-y-80"></div>
 </div>
+<p class="text-4xl font-bold text-left">Welcome back, <span class="text-sky-500">Razka</span>!</p>
 @endsection
