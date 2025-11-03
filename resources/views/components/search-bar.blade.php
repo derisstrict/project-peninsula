040626422dfbee4ea694d38f5ef026067fdc2bd0
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-11-04 01:05:35 +0800

    Added accent to search bar

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -1,5 +1,5 @@
 <div class="relative flex bg-light-primary/5 w-fit items-center pl-3 rounded-xl p-2 gap-2 dark:bg-dark-primary/5">
-    <x-icon icon="search" class="text-lime-600" width="24px" height="24px" stroke="currentColor" viewBox="0 0 24 24" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg"></x-icon>
+    <x-icon icon="search" class="{{ $accent }}" width="24px" height="24px" stroke="currentColor" viewBox="0 0 24 24" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg"></x-icon>
     <input type="text" id="search" class="w-80 outline-none" placeholder="Search">
     <div class="absolute right-5 text-light-primary/50 bg-light-primary/5 px-2 rounded-lg dark:text-dark-primary/50 dark:bg-dark-primary/5">t</div>
 </div>
