e21450af3606d2379b43bfd9da7dbb08e10e724d
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-11-10 18:45:17 +0800

    Renamed the Icon component class

--- resources/views/components/blogs-card-grid.blade.php
+++ resources/views/components/blogs-card-grid.blade.php
@@ -4,10 +4,10 @@
     <div class="bg-lime-600/25 outline-2 outline-lime-600/50 w-fit rounded-lg px-2 text-sm">Other</div>
     <div class="flex items-center justify-center h-5">
         <p class="text-sm mr-auto">{{ $date }}</p>        
-        <x-icon x-show="open" x-transition:enter="transition ease-out duration-150"
+        <x-local-icon x-show="open" x-transition:enter="transition ease-out duration-150"
         x-transition:enter-start="opacity-0 translate-y-5"
         x-transition:enter-end="opacity-100 scale-100"
         x-transition:leave="transition ease-in duration-100" x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 translate-y-5"  icon="arrow"  width="24px" height="24px" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
-        </x-icon>      
+        </x-local-icon>      
     </div>
 </a>
