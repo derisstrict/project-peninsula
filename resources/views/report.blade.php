745045e447c6e82df26b4ce8b5523472b2b3af0c
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-12-08 18:55:03 +0800

    Removed unused form

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -18,36 +18,9 @@
     </div>
     <div class="flex flex-col gap-3 w-full">
         <p class="font-bold">{{ __('report.form_title') }}</p>
-        {{-- <div class="flex gap-3">
-            <div class="flex bg-light-primary/5 w-fit items-center pl-3 rounded-xl p-2 gap-2 dark:bg-dark-primary/5">
-                <x-local-icon icon="user" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg"></x-local-icon>
-                <input type="text" id="name" class="w-80 outline-none" placeholder="{{ __('report.name') }}">
-            </div>
-            <div class="flex bg-light-primary/5 w-fit items-center pl-3 rounded-xl p-2 gap-2 dark:bg-dark-primary/5">
-                <x-local-icon icon="email" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg"></x-local-icon>
-                <input type="text" id="email" class="w-80 outline-none" placeholder="{{ __('report.email') }}">
-            </div>
+        <div class="bg-gray-50 px-8 py-8 rounded-xl dark:bg-gray-50/5">
+            @livewire('report-form')
         </div>
-        <div class="flex flex-col bg-light-primary/5 w-full pl-3 rounded-xl p-2 gap-2 dark:bg-dark-primary/5">
-            <div class="flex gap-2 items-center">
-                <x-local-icon icon="file-pencil" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg"></x-local-icon>
-                <p>{{ __('report.report') }}</p>
-            </div>
-            <textarea type="text" id="name" rows="8" class="w-full  rounded-lg outline-none bg-light-primary/5 p-2 resize-none dark:bg-dark-primary/5"></textarea>
-        </div>
-        <div class="flex items-center gap-3">
-            <button class="btn-primary p-3 w-fit">
-                <x-local-icon icon="attachment" height="16px" width="16px" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-	            viewBox="0 0 512 512" fill="currentColor" stroke="currentColor"  xml:space="preserve"></x-local-icon>
-                {{ __('report.attachments') }}
-            </button>            
-            <p class="text-xs">{{ __('report.attachments_note') }}</p>
-            <button class="btn-primary p-3 ml-auto">
-                <x-local-icon icon="tick" width="20px" height="20px" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
-                </x-local-icon>
-                {{ __('report.submit') }}</button>
-        </div> --}}
-        @livewire('report-form')
     </div>
 </div>
 @endsection
