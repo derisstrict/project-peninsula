6f1353233a69f12b13d7acdbd4abb0a050e48a50
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-12-08 18:56:26 +0800

    Added several new localization to blogs and pagination

--- resources/views/blogs-detail.blade.php
+++ resources/views/blogs-detail.blade.php
@@ -7,7 +7,7 @@
     <svg width="16px" height="16px" class="rotate-225" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
     <path d="M7 17L17 7M17 7H8M17 7V16" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
-    <p class="text-sm">Back</p> 
+    <p class="text-sm">{{ __('common.back') }}</p> 
 </button>
 <div class="flex flex-col gap-2 mt-5"> 
     <img class="rounded-xl self-center w-500 h-150 object-cover" src={{ asset('storage/' . $blogs->gambar_blog) }} alt="blog-image">
@@ -19,7 +19,7 @@
     </div>
 </div>
 <div class="flex flex-col mt-15 gap-5">
-    <p class="text-4xl font-semibold">More <span class="text-lime-600">Articles</span></p>
+    <p class="text-4xl font-semibold">{!! __('blogs_detail.title', ['accent' => '<span class="text-lime-600"> '. __('blogs_detail.title_accent') .' </span>']) !!}</p>
     <div class="grid grid-cols-3">
         @foreach ($more_articles as $more_article)
             <x-blogs-card-grid href="/blogs/{{ $more_article->id }}/{{ $more_article->slug}}" 
@@ -30,7 +30,7 @@
     </div>
     <div class="flex justify-center">
         <a href="/blogs" class="btn-primary p-3">
-            <p class="text-sm">Check out more articles</p>
+            <p class="text-sm">{{ __('blogs_detail.more') }}</p>
             <svg width="16px" height="16px" class="rotate-45" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M7 17L17 7M17 7H8M17 7V16" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
