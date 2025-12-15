008324d75aacae72e3755619e50a456de8109e2e
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-12-15 21:01:34 +0800

    Move popup marker content to top

--- resources/views/components/map-marker.blade.php
+++ resources/views/components/map-marker.blade.php
@@ -1,7 +1,7 @@
 <script>
     var popupContent = `
-        <div class="flex items-center mr-2 ml-4 my-2">
-            <div class="flex flex-col max-w-2xl w-full">
+        <div class="flex items-start mr-2 ml-4 my-2">
+            <div class="flex flex-col max-w-2xl w-full mr-2">
                 <h1 class="font-bold text-2xl text-color-accent">{{ $title }}</h1>
                 <span class="mt-2">{{ $teaser }}</span>
                 <div @click='openMapPopup = true, title = @json($title), desc = @json($desc), images = @json($images), note = @json($note), alt = @json($alt)' class="flex items-center gap-2 cursor-pointer bg-lime-600/15 w-fit rounded-lg py-1 px-2 mt-3 transition-colors hover:bg-lime-600/30">
