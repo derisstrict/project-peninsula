346fab39c1f87da69bc21fcaa1791c2d55550682
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-12-16 00:32:03 +0800

    Changed localization menu's name in admin

--- app/Filament/Resources/LanguageLines/LanguageLineResource.php
+++ app/Filament/Resources/LanguageLines/LanguageLineResource.php
@@ -19,7 +19,9 @@ class LanguageLineResource extends Resource
 {
     protected static ?string $model = LanguageLine::class;
 
-    protected static ?string $modelLabel = 'Localization';
+    protected static ?string $modelLabel = 'Lokalisasi';
+
+    protected static ?string $pluralModelLabel = 'Lokalisasi';
 
     protected static string | UnitEnum | null $navigationGroup = 'Settings';
 
