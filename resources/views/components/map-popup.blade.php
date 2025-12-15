df3689c93e3b9bdc23fca4d01ca5e9b72adda50c
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-12-15 19:34:25 +0800

    Improves responsiveness in map popup
    Not the best implementation for the scrolling part, got no time

--- resources/views/components/map-popup.blade.php
+++ resources/views/components/map-popup.blade.php
@@ -28,23 +28,22 @@
     }'
         x-init='$watch("images", value => { imgs = value });
     $watch("openMapPopup", value => { reset(runInterval) })'
-        @click.outside="openMapPopup = false"
-        class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(circle,_rgba(94,165,0,1)_-600%,_rgba(238,238,238,1)_100%)] rounded-2xl shadow-lg w-[90%] max-w-[1000px] max-h-[100vh] scrollbar-hide border-2 border-dark-primary px-8 py-8 dark:bg-[radial-gradient(circle,_rgba(94,165,0,1)_-600%,_rgba(8,16,7,1)_100%)] dark:border-light-primary">
+        @click.outside="openMapPopup = false, disableScrollOnBody(false)"
+        class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(circle,_rgba(94,165,0,1)_-600%,_rgba(238,238,238,1)_100%)] rounded-2xl shadow-lg w-[90%] max-w-[1000px] max-h-[100vh] border-2 border-dark-primary px-8 py-4 dark:bg-[radial-gradient(circle,_rgba(94,165,0,1)_-600%,_rgba(8,16,7,1)_100%)] dark:border-light-primary md:py-8">
         <div class="relative flex flex-row gap-2">
-            <p class="text-xl font-semibold text-color-accent">{{ __('maps.about_place') }}</p>
+            <p class="text-lg font-semibold text-color-accent md:text-xl">{{ __('maps.about_place') }}</p>
         </div>
 
-        <div class="flex">
-            <h2 class="text-4xl font-semibold mb-1" x-text="title"></h2>
-            <button @click="openMapPopup = false" class="btn-primary px-4 py-2 ml-auto cursor-pointer">
+        <div class="flex flex-col md:flex-row">
+            <h2 class="text-2xl font-semibold mb-1 md:text-4xl" x-text="title"></h2>
+            <button @click="openMapPopup = false, disableScrollOnBody(false)" class="!hidden btn-primary px-4 py-2 ml-auto cursor-pointer md:!flex">
                 <x-local-icon icon="x-cross" width="16px" height="16px" viewBox="0 0 200 200" fill="currentColor"
                     stroke="currentColor" xmlns="http://www.w3.org/2000/svg"></x-local-icon>
             </button>
         </div>
-
-        <div class="mt-5 flex flex-row gap-3">
+        <div id="modalContent" class="flex flex-col p-2 overflow-y-auto max-h-[560px] gap-3 lg:flex-row lg:mt-5" onmouseenter="disableScrollOnBody(true)">
             <div class="relative h-fit">
-                <div class="relative w-80 h-90 rounded-2xl overflow-hidden">
+                <div class="relative w-full h-[300px] overflow-hidden rounded-2xl lg:w-80 lg:h-[350px]">
                     <template x-for="(img, index) in imgs">
                         <img :src="'/img/' + img" :class="active == index ? 'opacity-100' : 'opacity-0'"
                             class="absolute inset-0 h-full w-full object-cover transition duration-700"
@@ -64,18 +63,30 @@
 
             <div class="flex flex-col gap-5">
                 <div class="w-fit max-w-4xl bg-light-primary/5 rounded-xl p-5 dark:bg-dark-primary/5">
-                    <p class="text-xl" x-text="desc"></p>
+                    <p class="text-md md:text-xl" x-text="desc"></p>
                 </div>
                 <div class="infobox-accent p-5 gap-3 !w-full">
                     <x-local-icon icon="info" class="text-color-accent w-fit min-w-[20px] max-w-[24px]" width="24px" height="24px"
                         viewBox="0 0 24 24" fill="none" stroke="currentColor"
                         xmlns="http://www.w3.org/2000/svg"></x-local-icon>
                     <div>
-                        <p class="font-semibold">{{ __('common.note') }}</p>
-                        <p x-text="note"></p>
+                        <p class="text-md font-semibold md:text-xl">{{ __('common.note') }}</p>
+                        <p class="text-md md:text-xl" x-text="note"></p>
                     </div>
                 </div>
             </div>
+            <button @click="openMapPopup = false, disableScrollOnBody(false)" class="btn-primary mt-2 items-center justify-center w-full px-4 py-4 ml-auto cursor-pointer md:!hidden">
+                <x-local-icon icon="x-cross" width="16px" height="16px" viewBox="0 0 200 200" fill="currentColor"
+                    stroke="currentColor" xmlns="http://www.w3.org/2000/svg"></x-local-icon>
+                <p>Close</p>
+            </button>
         </div>
     </div>
 </div>
+<script>
+    // Not the best way to do this, fix when in priority.
+    function disableScrollOnBody(disabled) {
+        const body = document.querySelector('body');
+        body.style.overflow = disabled ? 'hidden' : '';
+    }
+</script>
