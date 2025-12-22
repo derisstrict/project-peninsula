1500c57d0152e71f97f402804a1004fca7ef90c8
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-12-22 20:57:20 +0800

    Added height to image in blog's more article to make it consistent

--- resources/views/components/blogs-card-grid.blade.php
+++ resources/views/components/blogs-card-grid.blade.php
@@ -1,5 +1,5 @@
 <a x-data="{ open: false }" @mouseenter="open = true" @mouseleave="open = false" {{ $attributes }} class="flex flex-col gap-2 p-5 rounded-xl hover:bg-light-primary/5 dark:hover:bg-dark-primary/5 transition-colors">
-    <img class="rounded-lg w-100 object-cover" src="{{ $image }}" alt="{{ $alt }}">
+    <img class="rounded-lg w-100 h-70 object-cover" src="{{ $image }}" alt="{{ $alt }}">
     <p class="text-lg font-semibold">{{ $title }}</p>
     {{-- <div class="bg-color-accent/25 outline-2 outline-color-accent/50 w-fit rounded-lg px-2 text-sm">Other</div> --}}
     <div class="flex items-center justify-center h-5">
