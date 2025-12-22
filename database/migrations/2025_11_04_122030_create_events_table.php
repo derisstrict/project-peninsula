639e13d8c6adc3987047864764c59a8df9f761b7
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-12-22 20:55:31 +0800

    Removed slug unique constraint from events table

--- database/migrations/2025_11_04_122030_create_events_table.php
+++ database/migrations/2025_11_04_122030_create_events_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('tanggal_selesai', 50)->nullable(false);
             $table->text('deskripsi_event')->nullable(false);
             $table->string('judul_event', 100)->nullable(false);
-            $table->string('slug', 100)->nullable(false)->unique();
+            $table->string('slug', 100)->nullable(false);
             $table->string('gambar_event', 100)->nullable(false);
             $table->string('alt_gambar', 100)->nullable(true);
             $table->string('nama_penyelenggara', 50)->nullable(true);
