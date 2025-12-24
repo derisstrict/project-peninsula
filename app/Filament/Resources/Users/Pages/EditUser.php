ca55fb1f13bd5386f617713d16568bac43b202ce
derisstrict <94850683+derisstrict@users.noreply.github.com>
2025-12-24 23:47:55 +0800

    Added bypass email verification only for superadmin

--- app/Filament/Resources/Users/Pages/EditUser.php
+++ app/Filament/Resources/Users/Pages/EditUser.php
@@ -3,9 +3,11 @@
 namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Icons\Heroicon;
 
 class EditUser extends EditRecord
 {
@@ -16,6 +18,16 @@ class EditUser extends EditRecord
         return [
             ViewAction::make(),
             DeleteAction::make(),
+            Action::make('Verifikasi email')
+            ->icon(Heroicon::Envelope)
+            ->requiresConfirmation()
+            ->modalHeading('Bypass Verifikasi Email')
+            ->modalDescription('Apakah anda yakin ingin melakukan bypass verifikasi email?')
+            ->modalSubmitActionLabel('Iya')
+            ->modalCancelActionLabel('Tidak')
+            ->action(function ($record) {
+                $record->markEmailAsVerified();
+            }),
         ];
     }
 }
