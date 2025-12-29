73b8fa151b7b74b24cf40072b651083b3a2a7f53
Wibisana7265 <sanawibi57@gmail.com>
2025-12-29 21:44:25 +0800

    benerin bug

--- app/Filament/Resources/FotoVideos/Pages/CreateFotoVideos.php
+++ app/Filament/Resources/FotoVideos/Pages/CreateFotoVideos.php
@@ -5,7 +5,7 @@ namespace App\Filament\Resources\FotoVideos\Pages;
 use App\Filament\Resources\FotoVideos\FotoVideoResource;
 use Filament\Resources\Pages\CreateRecord;
 
-class CreateFotoVideo extends CreateRecord
+class CreateFotoVideos extends CreateRecord
 {
     protected static string $resource = FotoVideoResource::class;
 }
