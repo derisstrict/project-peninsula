33171e60a10a67e06dded46b72ad91e1006deb95
derisstrict <94850683+derisstrict@users.noreply.github.com>
2026-01-03 14:16:32 +0800

    Removed view button
    another one for consistency

--- app/Filament/Resources/WhitelistUsers/Pages/EditWhitelistUser.php
+++ app/Filament/Resources/WhitelistUsers/Pages/EditWhitelistUser.php
@@ -14,7 +14,7 @@ class EditWhitelistUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            ViewAction::make(),
+            // ViewAction::make(),
             DeleteAction::make(),
         ];
     }
