85ecdd2a0d9080b2e39720457f2ced54c05eb093
derisstrict <94850683+derisstrict@users.noreply.github.com>
2026-01-05 09:06:45 +0800

    Update BlogSeeder.php

--- database/seeders/BlogSeeder.php
+++ database/seeders/BlogSeeder.php
@@ -15,17 +15,17 @@ class BlogSeeder extends Seeder
     {
         Blog::create([
             'id_user' => '1',
-            'isi_blog' => 'The Nusa Dua, 27 Oktober 2025 - Suasana meriah mewarnai The Nusa Dua Festival (NDF) 2025 yang mengusung tema “Beauty Harmony”, di kawasan The Nusa Dua, Bali. Tercatat sebanyak 18.825 orang menikmati sajian musik, seni, dan budaya di tengah keindahan lanskap pesisir Pulau Peninsula pada 25-26 Oktober 2025. Diselenggarakan oleh InJourney Tourism Development Corporation (ITDC), anggota dari InJourney Group, festival ini menjadi bukti nyata kebangkitan event ikonik yang telah lama menjadi kebanggaan Bali.
+            'isi_blog' => "The Nusa Dua, October 27, 2025 - A festive atmosphere filled The Nusa Dua Festival (NDF) 2025, themed \"Beauty Harmony,\" in The Nusa Dua area, Bali. A total of 18,825 people enjoyed the music, art, and cultural offerings amidst the beautiful coastal landscape of Peninsula Island on October 25-26, 2025. Organized by InJourney Tourism Development Corporation (ITDC), a member of the InJourney Group, the festival demonstrated the resurgence of this iconic event, a long-standing source of Balinese pride.
 
-            Sejak sore hingga malam hari, ribuan pengunjung memenuhi area Pulau Peninsula, The Nusa Dua untuk menikmati penampilan musisi nasional dan lokal, atraksi budaya Bali, seni “body painting” serta berbagai sajian kuliner dan produk UMKM lokal yang dikurasi khusus, serta tenant The Nusa Dua. Suasana hangat dan semarak menjadi cerminan harmoni antara masyarakat, seni, dan alam di salah satu destinasi pariwisata terbaik di Indonesia ini.
+            From afternoon until evening, thousands of visitors filled the Peninsula Island area of ​​The Nusa Dua to enjoy performances by national and local musicians, Balinese cultural attractions, body painting, and a variety of culinary delights, specially curated products from local MSMEs, and The Nusa Dua tenants. The warm and lively atmosphere reflected the harmony between community, art, and nature in one of Indonesia's top tourism destinations.
 
-            Suksesnya penampilan Bunga Citra Lestari (BCL) dan Kahitna di panggung utama turut diikuti oleh penampilan memukau dari para musisi lokal seperti Tika Pagraky, Navicula, dan Astera. Kolaborasi antara musisi nasional dan lokal ini menghadirkan energi positif yang membuat seluruh pengunjung larut dalam kemeriahan festival.
+            The successful performances of Bunga Citra Lestari (BCL) and Kahitna on the main stage were followed by stunning performances by local musicians such as Tika Pagraky, Navicula, and Astera. This collaboration between national and local musicians brought a positive energy that immersed all attendees in the festival's excitement.
 
-            Direktur Operasi ITDC, Troy Warokka, menyampaikan apresiasi atas antusiasme masyarakat yang begitu tinggi terhadap pelaksanaan festival tahun ini. “Kami sangat bersyukur melihat sambutan luar biasa dari masyarakat. Sebanyak 18.825 pengunjung hadir, menunjukkan bahwa Nusa Dua Festival tetap menjadi ikon kebanggaan yang dinantikan setiap tahun. Festival ini bukan hanya ajang hiburan, tetapi juga wadah untuk menampilkan harmoni antara budaya, musik, dan keindahan kawasanThe Nusa Dua,” ujar Troy.
+            ITDC's Director of Operations, Troy Warokka, expressed his appreciation for the high level of public enthusiasm for this year's festival. \"We are very grateful to see the extraordinary response from the public. A total of 18,825 visitors attended, demonstrating that the Nusa Dua Festival remains a proud icon and is eagerly awaited every year. This festival is not only an entertainment event, but also a platform to showcase the harmony between culture, music, and the beauty of the Nusa Dua area,\" said Troy.
 
-            The Nusa Dua Festival 2025 merupakan bentuk nyata komitmen ITDC untuk menghadirkan kembali event ikonik yang telah dikenal sejak tahun 1996 dengan nama Nusa Dua Fiesta. “Kami berkomitmen untuk terus menghadirkan kegiatan yang berdampak positif bagi masyarakat, pelaku UMKM, serta industri pariwisata nasional. Melalui festival ini, kami ingin memperkuat posisi The Nusa Dua sebagai destinasi berkelas dunia yang tetap dekat dengan masyarakat,” tambahnya.
+            The Nusa Dua Festival 2025 is a concrete manifestation of ITDC's commitment to bringing back this iconic event, known since 1996 as the Nusa Dua Fiesta. \"We are committed to continuing to present activities that have a positive impact on the community, MSMEs, and the national tourism industry. Through this festival, we want to strengthen The Nusa Dua's position as a world-class destination that remains close to the community,\" he added.
 
-            Festival yang berlangsung selama dua hari, 25-26 Oktober 2025, ini sukses menghadirkan beragam penampilan menarik dan kolaborasi lintas genre yang memukau, meninggalkan kesan mendalam bagi para pengunjung hingga malam penutupan.',
+            The two-day festival, October 25-26, 2025, successfully presented a variety of engaging performances and captivating cross-genre collaborations, leaving a lasting impression on visitors until the closing night.",
             'judul_blog' => '18,000 Visitors Flocked to the Nusa Dua Festival 2025 on Peninsula Island',
             'slug' => 'the-nusa-dua-festival-2025',
             'gambar_blog' => 'blogs/nusa_dua_festival.webp',
@@ -34,24 +34,23 @@ class BlogSeeder extends Seeder
 
         Blog::create([
             'id_user' => '1',
-            'isi_blog' => 'The Nusa Dua Festival 2025 yang digelar di Peninsula Island, Nusa Dua, Badung, pada Sabtu (25-26/10/2025). Festival kali ini menghadirkan konsep baru kolaborasi dari masyarakat lokal dengan pihak swasta.
-            Kegiatan ini dinilai memberikan dampak signifikan, tidak hanya terhadap pengembangan kawasan pariwisata Nusa Dua, tetapi juga bagi aspek sosial budaya dan kebangkitan ekonomi masyarakat.
-            "Mungkin kalau bisa dilihat tidak ada merek asing yang masuk ke sini, namun bagaimana kami menyediakan di Nusa Dua Festival ini sebagai etalase, sebagai showcase untuk kebangkitan budaya, ekonomi, khususnya UMKM dan juga partisipasi masyarakat," ujar Direktur Operasional ITDC Troy Reza Warokka, kepada media, Sabtu (25/10/2025).
+            'isi_blog' => "The Nusa Dua Festival 2025 was held on Peninsula Island, Nusa Dua, Badung, on Saturday (October 25-26, 2025). This festival featured a new concept of collaboration between the local community and the private sector.
+            This event is considered to have a significant impact, not only on the development of the Nusa Dua tourism area, but also on the socio-cultural aspects and the economic revival of the community.
+            \"Perhaps, if you look at it, there are no foreign brands coming here, but we are providing this Nusa Dua Festival as a showcase, a showcase for cultural and economic revival, especially for MSMEs, and also community participation,\" said ITDC Operations Director Troy Reza Warokka to the media on Saturday (October 25, 2025).
 
-            Festival tahun ini diikuti oleh 30 tenant F&B, 17 tenant hotel dan fasilitas, serta 4 UMKM binaan ITDC yang mencakup sektor kuliner, kriya, perhiasan, dan paguyuban.
+            This year's festival was attended by 30 F&B tenants, 17 hotel and facility tenants, and four ITDC-assisted MSMEs spanning the culinary, craft, jewelry, and community sectors.
 
-            Selain menghadirkan berbagai produk lokal, festival ini juga menampilkan kolaborasi antara hotel dan banjar dalam parade budaya. Sebanyak 13 kontingen yang terdiri dari masing-masing perwakilan hotel dan banjar desa adat ikut ambil bagian dalam pawai budaya.
+            In addition to showcasing various local products, the festival also featured collaborations between hotels and the village community in a cultural parade. A total of 13 contingents, consisting of representatives from hotels and traditional village banjars, took part in the cultural parade.
 
-            "Kalau kita lihat hampir semua banjar ikut berpartisipasi, berkolaborasi dengan pihak swasta. Di mana swasta itu adalah tenan-tenan daripada ITDC, khususnya di Nusa Dua. Ada beragam hotel yang terlibat di situ dan tentunya tokoh masyarakat. Kita bisa lihat banyak tokoh masyarakat yang hadir," jelasnya.
+            \"Almost all the banjars participated, collaborating with the private sector. These private sectors are ITDC tenants, especially in Nusa Dua. Various hotels were involved, and of course, community leaders. We saw many community leaders in attendance,\" he explained.
 
-            Mengusung tema "Beauty Harmony", festival tahun ini menekankan pentingnya harmoni dan kolaborasi antara banjar-banjar dengan hotel-hotel. Dengan hal ini, ia ingin menunjukkan bahwa ITDC menjadi pihak yang inklusif untuk masyarakat sekitar.
+            Themed \"Beauty Harmony,\" this year's festival emphasized the importance of harmony and collaboration between banjars and hotels. He aimed to demonstrate ITDC's inclusive approach to the surrounding community.
 
-            "Jadi kami ingin menunjukkan bahwa Nusa Dua itu boleh dibilang eksklusif, tapi sebetulnya sangat inklusif. Jadi semua orang bisa masuk, semua orang bisa berkolaborasi. Dan semua orang bisa mendapat manfaat dengan aturan-aturan yang kita tetapkan," tambah General Manager ITDC The Nusa Dua I Made Agus Dwiatmika.
+            \"So, we want to show that Nusa Dua may be considered exclusive, but it's actually very inclusive. So, everyone can join, everyone can collaborate, and everyone can benefit from the rules we set,\" added I Made Agus Dwiatmika, General Manager of ITDC The Nusa Dua.
 
-            Acara yang sebelumnya dikenal dengan nama Nusa Dua Fiesta ini melibatkan 1.866 orang dalam penyelenggaraannya. Mulai dari peserta parade, anak-anak sekolah dasar yang menaripendet, pelakuUMKM dan paguyuban, peserta lomba lokal, hinggapecalang dari tiga desa adat yang menjaga keamanan acara.
-            Beragam kegiatan turut memeriahkan festival ini, seperti kompetisi penjor, lomba fotografi dan clay mask, body painting, hingga berbagai pertunjukan budaya yang menampilkan kreativitas masyarakat Bali.
+            The event, formerly known as the Nusa Dua Fiesta, involved 1,866 people. From parade participants to elementary school children dancing the pendet dance, MSMEs and community groups, local competition participants, and even the Pecalang (traditional Balinese dance group) from three traditional villages who maintained security, the festival featured a variety of activities, including a penjor competition, photography and clay mask contests, body painting, and various cultural performances showcasing the creativity of the Balinese people.
 
-            Sebagai puncak hiburan, hadir sejumlah artis ternama tanah air, yakni Bunga Citra Lestari (BCL) dan Tika Pagraky pada malam pembukaan, serta Kahitna, Navicula, dan Astera yang menutup rangkaian acara dengan meriah pada malam penutupan.',
+            The highlight of the entertainment was a number of renowned Indonesian artists, including Bunga Citra Lestari (BCL) and Tika Pagraky on the opening night, and Kahitna, Navicula, and Astera, who capped off the event with a festive atmosphere on the closing night.",
             'judul_blog' => 'Banjar and Hotel Collaboration Colors the Cultural Parade of The Nusa Dua Festival 2025',
             'slug' => 'parade-budaya-the-nusa-dua-festival-2025',
             'gambar_blog' => 'blogs/parade.jpg',
